214ff5bdfeb9d325592cb93f701dcfee84c38aac
estergcarvalho <ester.estudosti@gmail.com>
2024-01-31 17:52:37 -0300

    feat:Criacao de projeto spring back-end

---
+++ src/main/java/com/magazinestore/MagazinestoreApplication.java
@@ -0,0 +1,13 @@
+package com.magazinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazinestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazinestoreApplication.class, args);
+	}
+
+}
