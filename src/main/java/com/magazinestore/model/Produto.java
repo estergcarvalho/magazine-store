e68ca04c87a53346f689be88ca0512f87cd1b3d2
estergcarvalho <ester.estudosti@gmail.com>
2024-02-01 10:18:12 -0300

    refactor: ReAltAdicionando dependencia lombok e alteracao na classe produto

--- src/main/java/com/magazinestore/model/Produto.java
+++ src/main/java/com/magazinestore/model/Produto.java
@@ -4,12 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import lombok.Getter;
 
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "produto")
+@Getter
 public class Produto {
 
     @Id
@@ -19,45 +19,4 @@ public class Produto {
     private String descricao;
     private BigDecimal preco;
     private String marca;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
 }
