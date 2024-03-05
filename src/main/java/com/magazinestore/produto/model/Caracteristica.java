d89bca4110f5223db464919e0d9a8f9869911da0
estergcarvalho <ester.estudosti@gmail.com>
2024-03-04 21:55:08 -0300

    feat: Criacao de dominio para caracteristica do produto

--- src/main/java/com/magazinestore/produto/model/Caracteristica.java
+++ src/main/java/com/magazinestore/produto/model/Caracteristica.java
@@ -1,36 +1,32 @@
 package com.magazinestore.produto.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 @Entity
 @Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Produto {
+public class Caracteristica {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "produto_id", referencedColumnName = "id")
+    private Produto produto;
+
     private String nome;
     private String descricao;
-    private BigDecimal preco;
-    private String marca;
-
-    @OneToMany(mappedBy = "produto", cascade = CascadeType.ALL)
-    private List<Caracteristica> caracteristica;
 
 }
